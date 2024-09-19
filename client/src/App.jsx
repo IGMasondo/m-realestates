6cdb6a5bf57ae0fde66c85343d18ca9990c9c62f
IGMasondo <igmasondo@outlook.com>
2024-09-19 21:33:46 +0200

    Header search bar and links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      <h1>Masondo</h1>
-    </div>
+<BrowserRouter>
+<Header />
+<Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/About" element={<About/>} />
+    <Route path="/Sign-in" element={<SignIn/>} />
+    <Route path="/Sign-up" element={<SignUp/>} />
+    <Route path="/Profile" element={<Profile/>} />
+</Routes>
+</BrowserRouter>
   )
 }
 
